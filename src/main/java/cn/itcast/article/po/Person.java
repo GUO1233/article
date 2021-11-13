4df28694ba9978afe8d715021768c07c57773ce0
guo <283316914@qq.com>
2021-11-13 09:01:54 +0800

    测试 本地提交记录 1

---
+++ src/main/java/cn/itcast/article/po/Person.java
@@ -0,0 +1,15 @@
+package cn.itcast.article.po;
+
+/**
+ * @author guoyf
+ * @Description
+ * @date 2021/11/13 8:52
+ */
+public class Person {
+    private String name;
+    private String age;
+
+    public void tellSpreak(){
+        System.out.println("hello everyday");
+    }
+}
